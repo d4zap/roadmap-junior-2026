764c38bbc2541ee42c2eb55fc3cd3bec2091c2a0
d4zap <dsalazarlasso@gmail.com>
2026-06-26 16:49:18 -0500

    feat: ejercicio comparacion operador mayor o igual

---
+++ fase1-fundamentos/03-operadores/ejercicio2_comparacion.py
@@ -0,0 +1,11 @@
+edad=17
+edad2=18
+edad3=25
+
+operacion = edad>=18
+operacion2 = edad2>=18
+operacion3 = edad3>=18
+
+print("¿Puede entrar edad 17?",operacion)
+print("¿Puede entrar edad 18?",operacion2)
+print("¿Puede entrar edad 25?",operacion3)
