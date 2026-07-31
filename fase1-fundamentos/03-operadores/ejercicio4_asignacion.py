d9d624ba96812afa6bae4ca9ad8206e15a1db16c
d4zap <dsalazarlasso@gmail.com>
2026-07-31 14:54:11 -0500

    feat: ejercicio operadores de asignacion compuesta

---
+++ fase1-fundamentos/03-operadores/ejercicio4_asignacion.py
@@ -0,0 +1,15 @@
+
+
+puntos = 100
+
+puntos+=50
+print ("Despues de bonus:", puntos)
+
+puntos -=30
+print("Despues de penalizacion:", puntos)
+
+puntos *= 2
+print("Despues de duplicar:", puntos)
+
+puntos //=3
+print("Despues de dividir entre 3:", puntos)
