22a72d20f085286a5fe5107a98ae93dfe17fec60
d4zap <dsalazarlasso@gmail.com>
2026-07-31 20:21:23 -0500

    feat: ejercicio condicionales if elif else

---
+++ fase1-fundamentos/04-estructuras-control/ejercicio6_condicionales.py
@@ -0,0 +1,9 @@
+
+edad= int(input("que edad tienes? "))
+
+if edad<= 12:
+     print("eres un niño")
+elif edad >=13 and edad<18: 
+     print("eres un adolescente") 
+else:
+      print ("eres un adulto")
