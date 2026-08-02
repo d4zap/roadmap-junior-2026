2a6175d7a582545db258277c0e5cd4d43af77638
d4zap <dsalazarlasso@gmail.com>
2026-08-01 23:36:19 -0500

    feat: bucle while adivina el numero con mensaje de error

--- fase1-fundamentos/04-estructuras-control/ejercicio7_bucles.py
+++ fase1-fundamentos/04-estructuras-control/ejercicio7_bucles.py
@@ -8,3 +8,14 @@ print ("---numeros del 1 al 20---")
 for numero2 in range (1,21):  
     if numero2 % 2 == 0:
        print (numero2)
+
+
+print("---adivina el numero---")
+numero_secreto = 7
+intento = 0
+
+while intento != numero_secreto:
+    intento = int (input("Adivina el numero: "))
+    if intento != 7:
+        print("vuelve a intentarlo")
+print("Correcto")
