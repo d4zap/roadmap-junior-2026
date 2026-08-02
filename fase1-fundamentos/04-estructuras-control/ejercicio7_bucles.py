9d8a3966379800199fe976b461d5e4fe2e3ccc7c
d4zap <dsalazarlasso@gmail.com>
2026-08-01 23:20:32 -0500

    feat: ejercico bucle for numeros y pares

---
+++ fase1-fundamentos/04-estructuras-control/ejercicio7_bucles.py
@@ -0,0 +1,10 @@
+print ("---numeros del 1 al 10---")
+
+for numero in range (1,11):
+    print (numero)
+
+print ("---numeros del 1 al 20---")
+
+for numero2 in range (1,21):  
+    if numero2 % 2 == 0:
+       print (numero2)
