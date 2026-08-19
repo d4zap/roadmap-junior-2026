2a4fc7ee0619f68412ddafeb34e9e76cac433293
d4zap <dsalazarlasso@gmail.com>
2026-08-19 12:55:38 -0500

    feat: funciones con return e input usuario

--- fase1-fundamentos/04-estructuras-control/ejercicio8_funciones.py
+++ fase1-fundamentos/04-estructuras-control/ejercicio8_funciones.py
@@ -4,13 +4,13 @@ respuesta = input(" tienes identificacion (si/no): ")
 tienes_id = respuesta == "si" 
 
 
-def verificar_entrada (edad, id):
-   if edad>= 18 and id:
+def verificar_entrada (edad,tiene_id):
+   if edad>= 18 and tiene_id:
       return True
    else:
       return False
 
-resultado = verificar_entrada(edad, id)
+resultado = verificar_entrada(edad,tienes_id)
 
 if resultado:
    print ("Bienvenido, puede entrar")
